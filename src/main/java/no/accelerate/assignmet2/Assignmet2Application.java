108596863228bbc2235a3079c73cee1c1405860b
VKure <v-kure@hotmail.com>
2023-08-21 10:10:25 +0200

    Initial Commit, and SuperheroDb files

---
+++ src/main/java/no/accelerate/assignmet2/Assignmet2Application.java
@@ -0,0 +1,13 @@
+package no.accelerate.assignmet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignmet2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignmet2Application.class, args);
+    }
+
+}
---
+++ src/test/java/no/accelerate/assignmet2/Assignmet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.accelerate.assignmet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignmet2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
