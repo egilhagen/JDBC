f0ffe7e73cbf78abf0153d97cc72de02e8ae9ab2
Vilde Kure <v-kure@hotmail.com>
2023-08-21 14:28:55 +0200

    Fixed Connection

--- src/main/java/no/accelerate/assignmet2/Assignmet2Application.java
+++ src/main/java/no/accelerate/assignmet2/Assignmet2Application.java
@@ -3,12 +3,12 @@ package no.accelerate.assignmet2;
 import no.accelerate.assignmet2.dao.AssignmentDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignmet2Application {
-
+public class Assignmet2Application implements ApplicationRunner {
     final
     AssignmentDAO assignmentDAO;
 
@@ -18,10 +18,11 @@ public class Assignmet2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Assignmet2Application.class, args);
+        AssignmentDAO assignmentDAO = new AssignmentDAO();
     }
 
-
-    public void run(ApplicationArguments args) throws  Exception{
-        assignmentDAO.testConnection();
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        assignmentDAO.test();
     }
 }
--- src/main/java/no/accelerate/assignmet2/dao/AssignmentDAO.java
+++ src/main/java/no/accelerate/assignmet2/dao/AssignmentDAO.java
@@ -10,25 +10,24 @@ public class AssignmentDAO {
     @Value("${spring.datasource.url}")
     private String url;
     @Value("${spring.datasource.username}")
-    private String user;
+    private String username;
     @Value("${spring.datasource.password}")
     private String password;
-
 
     public AssignmentDAO() {
     }
 
-    public AssignmentDAO(String url, String user, String password) {
+    public AssignmentDAO(String url, String username, String password) {
         this.url = url;
-        this.user = user;
+        this.username = username;
         this.password = password;
     }
 
-    public void testConnection(){
-        try (Connection conn = DriverManager.getConnection(url, user, password);){
-            System.out.println("Connected");
+    public void test() {
+        try(Connection conn = DriverManager.getConnection(url, username,password);) {
+            System.out.println("Connected to Postgres...");
         } catch (SQLException e) {
-            System.out.println(e.getMessage());;
+            e.printStackTrace();
         }
     }
 
