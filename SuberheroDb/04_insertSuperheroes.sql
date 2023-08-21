8281217da12d95c3a48a45c2419037782375452a
VKure <v-kure@hotmail.com>
2023-08-21 12:34:11 +0200

    Fixed SuperheroesDb

--- SuberheroDb/04_insertSuperheroes.sql
+++ SuberheroDb/04_insertSuperheroes.sql
@@ -2,4 +2,4 @@ INSERT INTO superhero (super_name, super_alias, super_origin) VALUES
 ('Bruce Wayne','Batman','Dead parents'),
 ('Peter Parker','Spiderman','Radioactive Spider'),
 ('Tony Stark','Iron Man','Being Kidnapped'),
-('Carol Danvers','Danvers','Plane Crash and then kidnapped by aliens');
+('Carol Danvers','Captain Marvel','Plane Crash and then kidnapped by aliens');
