b2b33f093b723c050671f231b3c9b9ff2181bf56
VKure <v-kure@hotmail.com>
2023-08-21 12:00:42 +0200

    Fixed run-file

--- src/main/java/no/accelerate/assignmet2/runners/ApplicationRunner.java
+++ src/main/java/no/accelerate/assignmet2/runners/ApplicationRunner.java
@@ -2,7 +2,7 @@ package no.accelerate.assignmet2.runners;
 
 import org.springframework.boot.CommandLineRunner;
 
-public class ChinookRunner implements CommandLineRunner {
+public class ApplicationRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
