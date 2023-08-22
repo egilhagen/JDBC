274d90a976a78a8eee96ae5bddcf2239f18c2948
Vilde Kure <v-kure@hotmail.com>
2023-08-22 13:16:12 +0200

    added new Records (country, genre, spender)

---
+++ src/main/java/no/accelerate/assignmet2/dao/models/CustomerCountry.java
@@ -0,0 +1,5 @@
+package no.accelerate.assignmet2.dao.models;
+
+public record CustomerCountry ( String first_name,
+                                String last_name,
+                                String country ){ }
---
+++ src/main/java/no/accelerate/assignmet2/dao/models/CustomerGenre.java
@@ -0,0 +1,5 @@
+package no.accelerate.assignmet2.dao.models;
+
+public record CustomerGenre( String first_name,
+                             String last_name,
+                             String genre) { }
